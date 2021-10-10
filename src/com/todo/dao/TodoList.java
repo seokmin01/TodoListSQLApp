834c578e1e0030701f4429b75fadecbab5e0d225
seokmin01 <22000666@handong.edu>
2021-10-11 00:15:59 +0900

    importData 메서드 주석처리

--- src/com/todo/dao/TodoList.java
+++ src/com/todo/dao/TodoList.java
@@ -287,37 +287,37 @@ public class TodoList {
 		return list.indexOf(t);
 	}
 
-	public void importData(String filename) {
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(filename));
-			String line;
-			String sql = "insert into list (title, memo, category, current_date, due_date)"
-					+ " values (?, ?, ?, ?, ?);";
-
-			int records = 0;
-			while ((line = br.readLine()) != null) {
-				StringTokenizer st = new StringTokenizer(line, "##");
-				String category = st.nextToken();
-				String title = st.nextToken();
-				String description = st.nextToken();
-				String due_date = st.nextToken();
-				String current_date = st.nextToken();
-
-				PreparedStatement pstmt = conn.prepareStatement(sql);
-				pstmt.setString(1, title);
-				pstmt.setString(2, description);
-				pstmt.setString(3, category);
-				pstmt.setString(4, current_date);
-				pstmt.setString(5, due_date);
-				int count = pstmt.executeUpdate();
-				if (count > 0)
-					records++;
-				pstmt.close();
-			}
-			System.out.println(records + " records read!!");
-			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	public void importData(String filename) {
+//		try {
+//			BufferedReader br = new BufferedReader(new FileReader(filename));
+//			String line;
+//			String sql = "insert into list (title, memo, category, current_date, due_date)"
+//					+ " values (?, ?, ?, ?, ?);";
+//
+//			int records = 0;
+//			while ((line = br.readLine()) != null) {
+//				StringTokenizer st = new StringTokenizer(line, "##");
+//				String category = st.nextToken();
+//				String title = st.nextToken();
+//				String description = st.nextToken();
+//				String due_date = st.nextToken();
+//				String current_date = st.nextToken();
+//
+//				PreparedStatement pstmt = conn.prepareStatement(sql);
+//				pstmt.setString(1, title);
+//				pstmt.setString(2, description);
+//				pstmt.setString(3, category);
+//				pstmt.setString(4, current_date);
+//				pstmt.setString(5, due_date);
+//				int count = pstmt.executeUpdate();
+//				if (count > 0)
+//					records++;
+//				pstmt.close();
+//			}
+//			System.out.println(records + " records read!!");
+//			br.close();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 }
